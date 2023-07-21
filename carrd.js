783a4239f391fa95c2419fde2b58187a83a4ec6c
Harley <105566208+Neophxbia@users.noreply.github.com>
2023-07-20 22:57:47 -0500

    I HATE IT HERE
    help me please

---
+++ carrd.js
@@ -0,0 +1,27 @@
+function discordLink() {
+    open(URL = 'https://discord.gg/playcdu');
+};
+
+function spotifyLink() {
+    open(URL = 'https://open.spotify.com/user/n3kghzajux2qngi6jy130mf82?si=fc718aa0d7fa4012');
+};
+
+function twitterLink() {
+    open(URL = 'https://twitter.com/MadHatt1er');
+};
+
+function steamLink() {
+    open(URL = 'https://steamcommunity.com/id/MaddHattie/')
+}
+
+function pronounLink() {
+    open(URL = 'https://en.pronouns.page/@Harlz');
+};
+
+function carrdLink() {
+    open(URL = 'https://madhatt.carrd.co');
+};
+
+function rickRoll() {
+    open (URL = 'https://youtu.be/dQw4w9WgXcQ')
+}
